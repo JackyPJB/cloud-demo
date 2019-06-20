75db65954b3aa862ae0fbedcb7c34982ab2eb375
Peng Jianbiao <pengjianbiao@hotmail.com>
2019-06-20 09:19:55 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/cloud/demo/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.cloud.demo.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author PENGJB4
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ service-hi/src/main/java/com/cloud/demo/servicehi/controller/HiController.java
@@ -0,0 +1,25 @@
+package com.cloud.demo.servicehi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+*describe: 
+*
+*@author PENGJB
+*@date 2019-6-19 17:57
+*/
+@Controller
+public class HiController {
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+
+}
